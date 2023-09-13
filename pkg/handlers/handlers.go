87bb463775d5a30c55377276792a55d1519870e1
robert-gherlan <gherlanilierobert@gmail.com>
2023-09-13 10:06:41 +0300

    Passing data to HTML template pages.

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/robert-gherlan/go-webapp/pkg/config"
+	"github.com/robert-gherlan/go-webapp/pkg/models"
 	"github.com/robert-gherlan/go-webapp/pkg/render"
 )
 
@@ -29,10 +30,15 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler.
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the about page handler.
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again!"
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 
 	"github.com/robert-gherlan/go-webapp/pkg/config"
+	"github.com/robert-gherlan/go-webapp/pkg/models"
 )
 
 var (
@@ -23,8 +24,13 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
+// AddDefaultData used to add default data to the template.
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
 // RenderTemplate renders a template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 
 	if app.UseCache {
@@ -39,7 +45,10 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	}
 
 	buf := new(bytes.Buffer)
-	_ = t.Execute(buf, nil)
+
+	td = AddDefaultData(td)
+
+	_ = t.Execute(buf, td)
 
 	_, err := buf.WriteTo(w)
 	if err != nil {
